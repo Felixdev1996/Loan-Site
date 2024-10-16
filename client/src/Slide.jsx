480965af6c16b9cc3468d08642cd982a5324a0f3
git config --global user.name <adeosunadeshina8@gmail.com>
2024-10-16 08:18:50 -0700

    adesh

---
+++ client/src/Slide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Slide() {
+  return (
+    <div>
+      <h1 className='bg-slate-900 font-bold text-white h-[100vh]'>HELLO</h1>
+    </div>
+  )
+}
+
+export default Slide
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './Header'
+import Slide from './Slide'
+// import Header from './Header'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
+    <Slide/>
+    {/* <Header/> */}
   </StrictMode>,
 )
