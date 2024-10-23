3853afecd49c0a9b59877b0935c07a7d18f70ae3
git config --global user.name <adeosunadeshina8@gmail.com>
2024-10-23 15:28:25 +0100

    work

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,33 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-import {BrowserRouter,Routes,Route} from 'BrowserRouter'
-import Header from './UI/HomeComponents/Header'
-=======
 import Header from './SecondaryComponents/Header'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
->>>>>>> 7d8f5c0427f122a09bdcac0418aada708f4d31dd
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    
     <BrowserRouter>
-<<<<<<< HEAD
     <Header/>
       <Routes>
-        
         <Route/>
 
       </Routes>
-=======
-
-    <Routes>
-      <Header/>
-      <Route/>
-    </Routes>
->>>>>>> 7d8f5c0427f122a09bdcac0418aada708f4d31dd
     </BrowserRouter>
   </StrictMode>,
 )
