880edcd4e34f99f0626f62516dd8dc84bb76ddb4
Felixdev1996 <felixchizoba01@gmail.com>
2024-10-23 13:22:37 +0100

    Felix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Slide from './Slide'
-// import Header from './Header'
+import {BrowserRouter,Routes,Route} from BrowserRouter
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Slide/>
-    {/* <Header/> */}
+    <BrowserRouter>
+      <Routes>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+    
   </StrictMode>,
 )
