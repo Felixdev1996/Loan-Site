893a3dbcbbca329d601d932a507344b0244a8368
Felixdev1996 <felixchizoba01@gmail.com>
2024-10-23 13:47:33 +0100

    intall

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter,Routes,Route} from BrowserRouter
+import {BrowserRouter,Routes,Route} from 'BrowserRouter'
+import Header from './SecondaryComponents/Header'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    
+    <BrowserRouter> 
+    <Header/>
       <Routes>
         <Route/>
       </Routes>
