5640fb41db55cf8a1a3d509c1cd42e7047c5b28c
git config --global user.name <adeosunadeshina8@gmail.com>
2024-10-26 20:57:02 -0700

    work

---
+++ client/src/SecondaryComponents/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-blue-950 h-[300px] '>
+      
+    </div>
+  )
+}
+
+export default Footer
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './SecondaryComponents/Header'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Footer from './SecondaryComponents/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    
     <BrowserRouter>
     <Header/>
+    <Footer/>
       <Routes>
         <Route/>
 
