7adad443314bc2ab1a964947bd80a68449c906a8
Felixdev1996 <felixchizoba01@gmail.com>
2024-10-28 20:08:54 +0100

    pushed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,16 +4,30 @@ import './index.css'
 import Header from './SecondaryComponents/Header'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Footer from './SecondaryComponents/Footer'
+import Home from './SecondaryComponents/Home'
+import About from './AboutComponents/About'
+import Contact from './ContactComponents/Contact'
+import Loan from './LoanprogramComponents/Loan'
+import Pages from './PageComponents/Pages'
+import Accept from './AcceptanceComponents/Accept'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Footer/>
     <BrowserRouter>
     <Header/>
       <Routes>
-        <Route/>
+      
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/loan' element={<Loan/>}/>
+        <Route path='/pages' element={<Pages/>}/>
+        <Route path='/accept' element={<Accept/>}/>
+
+
 
       </Routes>
+      <Footer/>
     </BrowserRouter>
   </StrictMode>,
 )
